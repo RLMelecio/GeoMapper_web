7313800c23cadb7bf8748e2f94b17e86e7c679ab
cyaegha811 <roland.meleciojr@hcdc.edu.ph>
2024-07-25 12:25:45 +0800

    feat: login, home, signp pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,8 @@ export default function Home() {
     return (
         <div className="flex bg-[#fff]">
             <Sidebar/>
-            <main className="pl-5 text-black">
-                <h1 className="font-bold text-[32px]">Wally Bold ni nga font weight.</h1>
-                <h2 className="font-medium text-[21px]">Nalingaw na nuon kog design sa admin panel.</h2>
+            <main className="pl-5 ">
+                
             </main>
         </div>
     )
